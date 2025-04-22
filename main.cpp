7ab17459fd7ce59632fbf8c45a7ee45f80893e16
CodeWarriorInspire <2430442851@qq.com>
2025-04-23 00:21:54 +0800

    master

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    // This is a simple C++ program that prints "hello world" to the console.
+    cout<<"hello world"<<endl;
+    return 0;
+}
